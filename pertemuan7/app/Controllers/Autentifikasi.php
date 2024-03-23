fd28569ef235a83ddbcef72dc7e3a5273c4248f9
FikalAhmad <fikaalm26@gmail.com>
2024-03-23 21:55:18 +0700

    fix Autentifikasi

--- pertemuan7/app/Controllers/Autentifikasi.php
+++ pertemuan7/app/Controllers/Autentifikasi.php
@@ -8,23 +8,41 @@ class Autentifikasi extends BaseController
 {
     public function index()
     {
+        $session = \Config\Services::session();
         // Check if user is already logged in
-        if (session()->get('email')) {
+        if ($session->get('email')) {
             return redirect()->to('user');
         }
 
-        $validation = \Config\Services::validation();
+        $form_validation = \Config\Services::validation();
 
-        $validation->setRules([
-            'email' => 'required|valid_email',
-            'password' => 'required'
-        ]);
-
-        if (!$validation->withRequest($this->request)->run()) {
+        $form_validation->setRules([
+            'email' => [
+              'label' => 'Alamat Email',
+              'rules' => 'required|valid_email',
+              'errors' => [
+                  'required' => 'Email Harus Diisi!!',
+                  'valid_email' => 'Email Tidak Benar!!'
+                  ]
+                ],
+            'password' => [
+              'label' => 'Password',
+              'rules' => 'required|trim',
+              'errors' => [
+                  'required' => 'Password harus diisi'
+                  ]
+                ],
+                ]
+        );
+        
+        if (!$form_validation->withRequest($this->request)->run()) {
             $data['judul'] = 'Login';
             $data['user'] = '';
-
-            return view('templates/aute_header', $data) . view('autentifikasi/login') . view('templates/aute_footer');
+            
+            echo view('templates/aute_header', $data);
+            echo view('autentifikasi/login');
+            echo view('templates/aute_footer');
+            
         } else {
             return $this->_login();
         }
@@ -32,41 +50,41 @@ class Autentifikasi extends BaseController
 
     private function _login()
     {
+        $session = \Config\Services::session();
         $email = $this->request->getPost('email');
-        $password = $this->request->getPost('password') ?? "";
+        $password = $this->request->getPost('password'); 
 
         $modelUser = new ModelUser();
-        $user = $modelUser->where('email', $email)->first();
-        $user_password = implode($user['password']);
-
+        $user = $modelUser->cekData($email);
+        
         if ($user) {
             if ($user['is_active'] == 1) {
-                if (password_verify($password, $user_password)) {
+                if ($password == $user['password']) {
                     $data = [
                         'email' => $user['email'],
                         'role_id' => $user['role_id']
                     ];
 
-                    session()->set($data);
+                    $session->set($data);
 
                     if ($user['role_id'] == 1) {
                         return redirect()->to('admin');
                     } else {
                         if ($user['image'] == 'default.jpg') {
-                            session()->setFlashdata('pesan', '<div class="alert alert-info alert-message" role="alert">Silahkan Ubah Profile Anda untuk Ubah Photo Profil</div>');
+                            $session->setFlashdata('pesan', '<div class="alert alert-info alert-message" role="alert">Silahkan Ubah Profile Anda untuk Ubah Photo Profil</div>');
                         }
                         return redirect()->to('user');
                     }
                 } else {
-                    session()->setFlashdata('pesan', '<div class="alert alert-danger alert-message" role="alert">Password salah!!</div>');
+                    $session->setFlashdata('pesan', '<div class="alert alert-danger alert-message" role="alert">Password salah!!</div>');
                     return redirect()->to('autentifikasi');
                 }
             } else {
-                session()->setFlashdata('pesan', '<div class="alert alert-danger alert-message" role="alert">User belum diaktifasi!!</div>');
+                $session->setFlashdata('pesan', '<div class="alert alert-danger alert-message" role="alert">User belum diaktifasi!!</div>');
                 return redirect()->to('autentifikasi');
             }
         } else {
-            session()->setFlashdata('pesan', '<div class="alert alert-danger alert-message" role="alert">Email tidak terdaftar!!</div>');
+            $session->setFlashdata('pesan', '<div class="alert alert-danger alert-message" role="alert">Email tidak terdaftar!!</div>');
             return redirect()->to('autentifikasi');
         }
     }
