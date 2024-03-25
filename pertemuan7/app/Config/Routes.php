707888778df6c1e27e797ccdc17da863be399216
FikalAhmad <fikaalm26@gmail.com>
2024-03-25 11:31:20 +0700

    fix routes

--- pertemuan7/app/Config/Routes.php
+++ pertemuan7/app/Config/Routes.php
@@ -6,3 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Autentifikasi::index');
+$routes->post('/autentifikasi', 'Autentifikasi::index');
+$routes->get('/admin', 'Admin::index');
