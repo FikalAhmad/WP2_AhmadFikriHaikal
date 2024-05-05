2011042d996a23ae774fb77c21ca1458da855eec
FikalAhmad <fikaalm26@gmail.com>
2024-05-05 11:38:39 +0700

    test: bump

--- pertemuan2/app/Models/Model_Latihan1.php
+++ pertemuan2/app/Models/Model_Latihan1.php
@@ -8,7 +8,8 @@ class Model_Latihan1 extends Model
 {
   public $nilai1, $nilai2, $hasil;
 
-  public function jumlah($n1 = null, $n2 = null){
+  public function jumlah($n1 = null, $n2 = null)
+  {
     $this->nilai1 = $n1;
     $this->nilai2 = $n2;
     $this->hasil = $this->nilai1 + $this->nilai2;
--- pertemuan2/app/Views/view-latihan1.php
+++ pertemuan2/app/Views/view-latihan1.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Latihan 1</title>
 </head>
+
 <body>
   Halo Kawan.. Yuk kita belajar web programming..!!!<br>
   Nilai 1 = <?= $nilai1 ?>
   Nilai 2 = <?= $nilai2 ?>
-  ini hasil dari pemodelan dengan metode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+  ini hasil dari pemodelan dengan metode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?><br>
 </body>
+
 </html>
